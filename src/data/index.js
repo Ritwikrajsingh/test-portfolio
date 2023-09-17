da685151aef82d6bf0f2d7276ecc9e307b41e680
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-09-18 00:46:53 +0530

    created data.js file

---
+++ src/data/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    emptySuResponses: ["An empty 'su'? Clearly, you're a master of the command line arts. 🧐",
+        "Empty 'su' commands are my favorite! They keep me on my virtual toes. 🦶",
+        "Empty 'su'? You've truly embraced the minimalist approach to computing. 👏",
+        "An empty 'su'... Pure poetry in the world of command lines. 📜",
+        "Ah, an empty 'su' A masterpiece of command line ambiguity. Bravo! 👑",
+        "Empty 'su'? You've stumbled into the virtual land of whimsy! 🌈",
+        "No 'su' command? It's a sign to dance like nobody's watching! 💃🕺",
+        "Empty 'su,' but brimming with virtual awesomeness! Let's make magic happen! ✨",
+        "An empty 'su'? Are you trying to achieve coding Nirvana, one command at a time? 🧘‍♂️",
+        "Empty 'su' commands are like debugging mysteries waiting to be solved. 🔍🐞",
+        "Empty 'su'? Clearly, you're optimizing for maximum suspense in your coding journey. ⏳",
+        "Empty 'su' commands are like code comments: full of potential but somewhat cryptic. 🤔📝",
+        "An empty 'su'? You've just written a null pointer exception in the coding universe. 🌌💥",
+        "When life gives you an empty 'su,' make it an opportunity for coding humor! 😄💻",
+        "An empty 'su'? A true hacker's way of keeping secrets in plain sight. 🕵️‍♂️🔒",
+        "Empty 'su' commands are like hidden Easter eggs in the world of coding. 🐣🍫"
+    ]
+}
