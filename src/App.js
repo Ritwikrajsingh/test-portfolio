5d6bc70c3c7593c944f52d56837b29c25c7f5e53
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-09-20 00:36:10 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css'
+import React, { useState, useEffect } from 'react';
+import Header from './compoenets/Header';
+import Prompt from './compoenets/Prompt';
+
+const App = () => {
+
+  const [user, setUser] = useState('guest');
+
+  const [theme, setTheme] = useState('dark');
+
+  const validCommands = ['help', 'su', 'ls', 'whoami', 'cat', 'clear', 'exit']
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  const changeUser = (username) => {
+    setUser(username);
+  }
+  const [content, setContent] = useState([<Header />])
+
+  useEffect(() => {
+    document.body.className = theme; // Set the body's className to the current theme
+  }, [theme]);
+
+  const themeLogo = theme === 'light' ? '/moon-tp.svg' : '/sun-tp.svg';
+
+  return (
+    <div className='App'>
+      <button id='theme' onClick={toggleTheme} style={{ backgroundColor: "transparent" }}>
+        <img src={themeLogo} alt={theme === 'light' ? 'Sun' : 'Moon'} />
+      </button>
+
+      {/* Content */}
+      <ul>
+        {content}
+        <Prompt user={user} setContent={setContent} changeUser={changeUser} />
+      </ul>
+    </div>
+  );
+};
+
+export default App;
