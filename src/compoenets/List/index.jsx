567e07c7fcf572a446aebb35be990ee2b9eb0730
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-09-24 12:43:40 +0530

    imporoved component structure

--- src/compoenets/List/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function List() {
-    const files = ['portfolio.md', 'skills.json', 'contact.md']
-    return (
-        <li className='files'>{files.map(file => <i>{file}</i>)}</li>
-    )
-}
