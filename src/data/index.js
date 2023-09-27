ad70f7470c130b57935240e0e2228d8cee2cdea7
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-09-27 12:52:52 +0530

    added emptySuMessages

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,39 @@
+let repositories;
+
+const fetchRepositories = async () => {
+    try {
+        const response = await fetch('https://api.github.com/users/ritwik/repos?per_page=100', { method: 'GET' });
+
+        if (response.status !== 200) {
+            console.error('Error:', response.status);
+            return;
+        }
+
+        const result = await response.json();
+        repositories = result.length;
+    } catch (error) {
+        console.error('Error:', error)
+    }
+};
+
+(async () => {
+    try {
+        await fetchRepositories()
+    } catch (error) {
+        console.error("Error:", error);
+        return ''
+    }
+})()
+
 module.exports = {
+    hostname: 'ritwik.s',
+    osIdentifiers: [
+        { identifier: "Win", name: "Windows" },
+        { identifier: "Mac", name: "macOS" },
+        { identifier: "Linux", name: "Linux" },
+        { identifier: "Android", name: "Android" },
+        { identifier: "iOS", name: "iOS" },
+    ],
     emptySuResponses: [
         "An empty 'su'? Clearly, you're a master of the command line arts. 🧐",
         "Empty 'su'? You've truly embraced the minimalist approach to computing. 👏",
@@ -15,5 +50,39 @@ module.exports = {
         "When life gives you an empty 'su,' make it an opportunity for coding humor! 😄💻",
         "An empty 'su'? A true hacker's way of keeping secrets in plain sight. 🕵️‍♂️🔒",
         "Empty 'su' commands are like hidden Easter eggs in the world of coding. 🐣🍫"
-    ]
+    ],
+    files: ['portfolio.md', 'skills.json', 'contact.md'],
+    skills: {
+        languages: ['javascript', 'python', 'java', 'html', 'css', 'C', 'C++', 'dart', 'micropython'],
+        frameworks: ['node.js', 'express.js', 'sequelize', 'react.js', 'next.js', 'flutter', 'bootstrap'],
+        databases: ['sql', 'nosql', 'postgresql', 'mongodb', 'mysql'],
+        tools: ['git', 'vscode', 'nvm', 'android-studio', 'sass', 'opencv'],
+        misc: ['linux', 'rest-api', 'jupyter-notebook', 'matplotlib', 'numpy', 'pandas', 'tkinter', 'kivy', 'pyqt', 'pygame', 'raspberry-pi']
+    },
+    contact: [
+        { title: "ritwikrajsingh2 @ twitter", url: "https://twitter.com/ritwikrajsingh2" },
+        { title: "ritwikrajsingh @ linkedin", url: "https://linkedin.com/in/ritwikrajsingh" },
+        { title: "ritwik_raj_singh @ telegram", url: "https://t.me/ritwik_raj_singh" },
+        { title: "the_demon @ discord", url: "https://discordapp.com/users/742510458811973712" },
+    ],
+
+    portfolio: async () => {
+        try {
+            await fetchRepositories();
+
+            const portfolioData = [
+                {
+                    title: 'GitHub',
+                    description: 'My github profile',
+                    additionalInfo: repositories ? repositories > 1 ? `${repositories} repos` : `${repositories} repo` : "", url: 'https://github.com/ritwikrajsingh'
+                },
+                { title: 'CodeDemon', description: 'My previous portfolio website', additionalInfo: 'which which doesn\'t work', url: 'https://ritwikrajsingh.github.io' },
+                { title: 'Tunica.tech', description: 'My page on their website', additionalInfo: null, url: 'https://tunicalabsmedia.com/meet/ritwik-raj-singh' },
+            ]
+            return portfolioData;
+        } catch (error) {
+            console.error("Error:", error);
+            return '';
+        }
+    }
 }
