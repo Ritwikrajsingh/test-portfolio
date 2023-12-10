0ffdca460f9ead0439e0e90eff4ef29c68687d1b
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-12-10 19:45:42 +0530

    used historyNavigator

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import './App.css'
-import React, { useState, useEffect } from 'react';
-import Header from './compoenets/Header';
-import Prompt from './compoenets/Prompt';
-import { emptySuResponses, files, hostname } from './data';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Header from "./compoenets/Header";
+import Prompt from "./compoenets/Prompt";
+import { emptySuResponses, files, hostname } from "./data";
 
 const App = () => {
-  const [user, setUser] = useState('guest');
-  const [theme, setTheme] = useState('dark');
+  const [user, setUser] = useState("guest");
+  const [theme, setTheme] = useState("dark");
+  const [history, setHistory] = useState([]);
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   const changeUser = (username) => {
     setUser(username);
-  }
-  const [content, setContent] = useState([<Header />])
-  // const [content, setContent] = useState([])
+  };
+  const [content, setContent] = useState([<Header />]);
+  // const [content, setContent] = useState([]);
 
   useEffect(() => {
     document.body.className = theme; // Set the body's className to the current theme
   }, [theme]);
 
-  const themeLogo = theme === 'light' ? '/moon-tpbg.svg' : '/sun-tp.svg';
+  const themeLogo = theme === "light" ? "/moon-tpbg.svg" : "/sun-tp.svg";
 
   return (
-    <div className='App'>
-      <button id='theme' onClick={toggleTheme} style={{ backgroundColor: "transparent" }}>
-        <img src={themeLogo} alt={theme === 'light' ? 'Sun' : 'Moon'} />
+    <div className="App">
+      <button
+        id="theme"
+        onClick={toggleTheme}
+        style={{ backgroundColor: "transparent" }}
+      >
+        <img src={themeLogo} alt={theme === "light" ? "Sun" : "Moon"} />
       </button>
       <ul>
         {content}
@@ -38,6 +43,8 @@ const App = () => {
           emptySuResponses={emptySuResponses}
           files={files}
           hostname={hostname}
+          history={history}
+          setHistory={setHistory}
         />
       </ul>
     </div>
